e1d8777d1b7819010b244ef2214afba0073cc98e
Jonathan Gamble <jdgamble555@gmail.com>
2025-02-02 11:38:21 -0600

    working sortable!

--- src/lib/use-sortable.svelte.ts
+++ src/lib/use-sortable.svelte.ts
@@ -3,12 +3,41 @@ import Sortable from 'sortablejs';
 export const useSortable = (
 	getter: () => HTMLElement | null,
 	options?: Sortable.Options
-
 ) => {
 	$effect(() => {
-		const sortable = getter();
-		if (sortable) {
-			Sortable.create(sortable, options);
-		}
+		const sortableEl = getter();
+		const sortable = sortableEl ?
+			Sortable.create(sortableEl, options)
+			: null;
+		return () => sortable?.destroy();
 	});
+}
+
+
+export function reorder<T>(
+	array: T[],
+	evt: Sortable.SortableEvent
+): $state.Snapshot<T>[] {
+
+	const workArray = $state.snapshot(array);
+
+	// get changes
+	const { oldIndex, newIndex } = evt;
+
+	if (oldIndex === undefined || newIndex === undefined) {
+		return workArray;
+	}
+	if (newIndex === oldIndex) {
+		return workArray;
+	}
+
+	// move elements
+	const target = workArray[oldIndex];
+	const increment = newIndex < oldIndex ? -1 : 1;
+
+	for (let k = oldIndex; k !== newIndex; k += increment) {
+		workArray[k] = workArray[k + increment];
+	}
+	workArray[newIndex] = target;
+	return workArray;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import Handle from '$lib/handle.svelte';
-	import { useSortable } from '$lib/use-sortable.svelte';
-
-	let sortable = $state<HTMLElement | null>(null);
+	import { reorder, useSortable } from '$lib/use-sortable.svelte';
 
 	let items = $state([
 		{
@@ -19,15 +17,21 @@
 		}
 	]);
 
+	let sortable = $state<HTMLElement | null>(null);
+
 	useSortable(() => sortable, {
 		animation: 200,
 		handle: '.my-handle',
-		ghostClass: 'opacity-0'
+		ghostClass: 'opacity-0',
+		onEnd(evt) {
+			items = reorder(items, evt);
+		}
 	});
 </script>
+
 <div class="hidden opacity-0"></div>
 <ul class="flex w-full list-none flex-col items-center" bind:this={sortable}>
-	{#each items as item (item.id)}
+	{#each items as item (item)}
 		<li class="m-2 flex w-32 items-center justify-center gap-5 border p-3">
 			<span>{item.text}</span>
 			<button type="button" class="my-handle outline-none">
@@ -36,3 +40,6 @@
 		</li>
 	{/each}
 </ul>
+<div class="flex justify-center">
+	<pre class="mt-5 w-fit border p-5">{JSON.stringify(items, null, 2)}</pre>
+</div>
